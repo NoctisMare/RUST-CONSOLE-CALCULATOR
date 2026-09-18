235d8d3c710f6b4b9da2b41c5f3f12fc208f1743
NoctisMare <mhz.lj.dev@proton.me>
2026-09-18 11:13:28 +0000

    feat: parse_operator() was added.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+// operator parser
+fn parse_operator(input: &str) -> Result<char, String> {
+    match input.trim() {
+        "+" => Ok('+'),
+        "-" => Ok('-'),
+        "*" => Ok('*'),
+        "/" => Ok('/'),
+        other => Err(format!("'{}' is not a valid operator.", other)),
+    }
 }
+
+fn main() {}
