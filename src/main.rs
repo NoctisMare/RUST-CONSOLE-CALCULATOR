e3dce0d3d861668d01f49848260e75b3a2ebb6d3
NoctisMare <mhz.lj.dev@proton.me>
2026-09-18 11:16:00 +0000

    feat: calculate module was added.

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,21 @@ fn parse_operator(input: &str) -> Result<char, String> {
     }
 }
 
+// calculate module
+fn calculate(a: f64, op: char, b: f64) -> Result<f64, String> {
+    match op {
+        '+' => Ok(a + b),
+        '-' => Ok(a - b),
+        '*' => Ok(a * b),
+        '/' => {
+            if b == 0.0 {
+                Err("Division by zero.".to_string())
+            } else {
+                Ok(a / b)
+            }
+        }
+        _ => unreachable!("[!] 'parse_operator()' should only return valid operators."),
+    }
+}
+
 fn main() {}
